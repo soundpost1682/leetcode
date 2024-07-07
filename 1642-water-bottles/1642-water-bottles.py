28e378db0d0bb5d8d102c055b50292bfaa598fa5
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2024-07-07 13:50:24 -0700

    Time: 31 ms (74.55%), Space: 16.3 MB (97.5%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.py
+++ 1642-water-bottles/1642-water-bottles.py
@@ -1,10 +1,3 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        tmp =0
-        while numBottles >= numExchange :
-            tmp += numExchange
-            numBottles -= numExchange
-
-            numBottles += 1
-        return tmp + numBottles
-        
+        return numBottles + (numBottles-1) // (numExchange-1)
