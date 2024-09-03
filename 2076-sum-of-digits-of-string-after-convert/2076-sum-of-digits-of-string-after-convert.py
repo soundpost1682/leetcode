d246f93b54ce057f741a33b5542ae9292922158f
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2024-09-03 15:09:16 -0700

    Time: 44 ms (16.14%), Space: 16.3 MB (93.83%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -2,6 +2,7 @@ class Solution:
     def getLucky(self, s: str, k: int) -> int:
         s = ''.join(str(ord(ch) - 96) for ch in s)
         for i in range(k):
-            s = str(sum(int(ch) for ch in s))
-        return int(s)
+            s = sum(map(int, str(s)))
+        return s
+        
 
