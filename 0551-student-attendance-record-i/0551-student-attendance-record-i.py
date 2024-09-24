17aea839351079aececad54dd37be9f85cca57cf
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2024-09-24 14:14:28 -0700

    Time: 35 ms (53.59%), Space: 16.4 MB (95.99%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,4 +1,16 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        return False if 'LLL' in s or s.count('A') >=2 else True
+        cntA, cntL = 0,0
+        for i in s:
+            if i=='A': 
+                cntA += 1
+                cntL = 0
+            elif i=='L':
+                cntL+=1
+            else : cntL=0
+
+            if cntA >=2 or cntL >=3:
+                return False
+        return True
         
+
