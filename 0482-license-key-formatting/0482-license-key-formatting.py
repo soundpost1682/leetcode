1ef27de449d56111fa6109a7cb7ca9373bf84afb
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2024-09-27 11:37:02 -0700

    Time: 41 ms (80.59%), Space: 18 MB (26.05%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.py
+++ 0482-license-key-formatting/0482-license-key-formatting.py
@@ -1,12 +1,14 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        tmp = s.replace('-','').upper()
-        ans1 = len(tmp) % k
-        reform = tmp[:ans1]
-        
-        for i in range(ans1, len(tmp), k):
-            if reform:
-                reform += '-'
-            reform += tmp[i:i+k]
-        return reform
-        
+        ans = []
+        cnt =0
+        s2 = s.replace('-', '').upper()
+        for i in reversed(range(len(s2))):
+            ans.append(s2[i])
+            cnt+=1
+            if cnt==k and i!=0:
+                ans.append('-')
+                cnt=0
+        return ''.join(ans[::-1])
+
+
