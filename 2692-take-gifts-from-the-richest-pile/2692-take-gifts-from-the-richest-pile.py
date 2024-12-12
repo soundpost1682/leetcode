a9a09610f3fc30ee4a9022b4dc55847b86a1fa92
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2024-12-12 15:29:51 -0800

    Time: 23 ms (36.98%), Space: 17.4 MB (22.1%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
@@ -1,9 +1,14 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        while k > 0:
-            maxpile = max(gifts)
-            i = gifts.index(maxpile)
-            gifts[i] = int(math.sqrt(maxpile))
-            k -= 1
+        while k:
+            gifts.sort(reverse=True)
+            pi = gifts[0]
+            gifts.pop(0)
+            gifts.append(int(sqrt(pi)))
+            k -=1
+            # maxpile = max(gifts)
+            # i = gifts.index(maxpile)
+            # gifts[i] = int(math.sqrt(maxpile))
+            # k -= 1
         return sum(gifts)
         
