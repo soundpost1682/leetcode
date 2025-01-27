92f10ec9f11312dab84d13420ce87bb78a58be4e
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2025-01-27 10:03:54 -0800

    Time: 29 ms (93.78%), Space: 17.6 MB (59.75%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,5 +1,4 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        tmp1 = command.replace('()', 'o')
-        tmp2 = tmp1.replace('(al)', 'al')
-        return tmp2
+        return command.replace('()','o').replace('(al)','al')
+        
