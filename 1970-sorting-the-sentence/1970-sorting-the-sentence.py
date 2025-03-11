643e59d3d38703e6023a9b9b11c4178e6c995e69
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2025-03-11 12:51:48 -0700

    Time: 35 ms (72.73%), Space: 17.7 MB (66.08%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.py
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -1,8 +1,10 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        tmp = [i[-1] + i[:-1] for i in s.split()]
-        tmp.sort()
-        ans = ''
+        tmp = s.split()
+        d = {}
         for i in tmp:
-            ans += i[1:] + ' '
-        return ans[:-1]
+            d[i[-1]] = i[:-1]
+            
+
+        return ' '.join(d[j] for j in sorted(d))
+
