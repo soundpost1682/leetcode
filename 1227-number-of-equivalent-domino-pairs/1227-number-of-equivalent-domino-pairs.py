cf9dcf50100670d55f7c4cad1060f0f3446161f9
soundpost1682 <158452720+soundpost1682@users.noreply.github.com>
2025-05-03 21:53:17 -0700

    Time: 4 ms (97%), Space: 24.2 MB (95.29%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -1,11 +1,10 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        cnt = Counter()
+        cnt = defaultdict(int)
         ans =0
         for i,v in dominoes:
-            tmp = tuple(sorted([i,v]))
-            if tmp in cnt:
-                ans+=cnt[tmp]
-            cnt[tmp] +=1
-        return ans
+            cnt[1<<i | 1<<v] +=1
+        for z in cnt.values():
+            ans += z * (z-1)
+        return ans // 2
         
